c6aeb7d05ec435de3d4dea22b0d9cf0b763e627b
Sidhant Sharma <121248718+IceBerG-15@users.noreply.github.com>
2022-12-28 18:55:39 +0530

    initial files

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+r = requests.get(URL)
+# print("encoding is", r.encoding)  # prints ISO-8859-1 (wrong)
+r.encoding = "utf-8"              # tells r.text to use utf-8
+soup = BeautifulSoup(r.text, "lxml")
+
+titles=soup.find_all(name='h3',class_='title')
+titles=titles[::-1]
+
+length=len(titles)
+with open('C:\\Python310\\codes\\projects\\100 must watch movies(WEB SCRAPPING)\Starting Code - 100 movies to watch start\\movies.txt','w') as file:  
+    for title in titles:
+        file.write(f'{title.text}\n')
